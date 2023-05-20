b6fa21cdb933b182fb07ef19b4d85cc12afacf7d
isaiahb <isaiahballah@gmail.com>
2023-05-20 10:49:53 +1000

    added clarity

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { NotificationProvider } from "./contexts/NotificationContext.tsx";
+import { clarity } from 'react-microsoft-clarity';
 
+clarity.init('h6njp07cdk');
+console.log("Clarity initialized");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
